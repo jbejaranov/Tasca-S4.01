5aa006e1fb08b059dcac5bbd06e9f2bb3ede78bf
Juan A. Bejarano <100026204+jbejaranov@users.noreply.github.com>
2023-01-30 10:55:44 +0100

    Update HelloWorldController.java

--- n1/S04T01N01/src/main/java/S04T01N01/controllers/HelloWorldController.java
+++ n1/S04T01N01/src/main/java/S04T01N01/controllers/HelloWorldController.java
@@ -1,4 +1,4 @@
-package cat.itacademy.barcelonactiva.BejaranoVizuete.Juan.s04.t01.n01.controllers;
+package S04T01N01.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
